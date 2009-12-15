8d67e43e9338a71daabd627dbfc64db744500a6a
Graham Cantin <gcantin@intrinsyx.com>
2009-12-14 16:54:00 -0800

    Added database logger module. Defaulting to disabled, as it's fairly slow.
    Signed-off-by: Graham Cantin <gcantin@intrinsyx.com>

--- ft_common/ft_logging_db.sh
+++ ft_common/ft_logging_db.sh
@@ -21,6 +21,12 @@
 # Strings
 # -----------
 
+SQLITE="${script_path}/ft_common/ft_sql64";
+
+db_log_filename="${script_path}/ft_logs/filetasker.sqlite"
+
+db_create_logs_table="CREATE TABLE main.ft_logs (id INTEGER PRIMARY KEY AUTOINCREMENT, sev INTEGER NOT NULL, time INTEGER NOT NULL, message TEXT NOT NULL);"
+
 # -----------
 # Paths
 # -----------
@@ -35,25 +41,23 @@
 
 # Start Debugging functions
 
-# Duplicate of console logging for now.
 message_output_to_database() # Logging to database
 { # Inputs: $1 - log_level, $2 - log_timestamp, $3 - log_message
-  # Check for Low severity events.
-  if [[ "${1}" -le "${MSG_NOTICE}" ]]; then # Check for low severity events
-    if [[ -e "${script_path}/ft_config/ft_config_narration.on" ]]; then # Narration is on.
-      echo -e "DB Narration (SEV:`sev_name ${1}`): ${3}";
-    else # Narration is off.
-      if [[ "${1}" -eq "${MSG_CONSOLE}" ]]; then # We only want messages marked CONSOLE.
-        echo -e "DB${3}"; # "Normal" Console messages *always* go to the terminal.
-      fi
-      if [[ -e "${script_path}/ft_config/ft_config_verbose.on" ]]; then # "Verbose" Console chatter enabled?
-        if [[ "${1}" -eq "${MSG_VERBOSE}" ]]; then # We only want messages marked VERBOSE.
-          echo -e "DB${3}"; # "Loud" Console messages sometimes go to the terminal.
-        fi
-      fi
-    fi
-  fi
+  #db_check_table_exists "ft_logs"; # Does our table exist?
+  db_sqlite_query "INSERT INTO main.ft_logs (sev,time,message) VALUES ('${1}','${2}','${3}');";
 }
+
+db_sqlite_query() { # Run a SQL Query
+  local db_result=$(${SQLITE} "${db_log_filename}" "${1}" 2>&1)
+  if [[ "$?" -gt 0 ]]; then echo ${db_result}; return $?; else return $?; fi }
+
+db_does_table_exist() { # Does a SQLLite Table named $1 exist?
+  local query="SELECT name FROM sqlite_master WHERE type='table' AND name='$1';"
+  local result=$(db_sqlite_query "$query")
+  if [[ "$result" == "$1" ]]; then return 0; else return 1; fi }
+
+db_check_table_exists() { db_does_table_exist "${1}";
+  if [[ $? -gt 0 ]]; then db_sqlite_query "${db_create_logs_table}"; else echo "TABLE OK"; fi }
 
 # End Debugging functions
 
